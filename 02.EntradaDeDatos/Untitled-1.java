79c555748e959560dc0003dbfc5a2e7ae0985945
AbnerYoav <a.hernandez.p244@edu.utc.mx>
2023-09-22 10:54:43 -0700

    Abner

---
+++ 02.EntradaDeDatos/Untitled-1.java
@@ -0,0 +1,28 @@
+//Debe de tener una clase
+//Public class    class
+
+
+class EntradaTexto{
+
+    public static void main(string[]  args){
+
+        String nombre;
+
+        System.out.println("Por favor ingrese tu nombre: ");
+
+        nombre = System.console().readline();
+    
+    System.out.println("Hola " + nombre + "Bienvenida a tronar");
+    
+    
+    
+    }
+
+
+
+
+
+
+
+    
+}
---
+++ 03.EstructurasControl.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+class EstrcutruasControl{
+
+    public static void main(string[] args){
+
+
+        Scanner entrada = new Scanner(System.in);
+
+        int opcion, numbinario, total, cantidadproducto, num1;
+        float precio, resultado = 0; compra = 0;
+        String nombreproducto;
+        String binario = "";
+
+        do{
+            System.out.println("Bienvenido al rpograma :D "):
+            System.out.println("Elija alguna opcion deseada: "):
+            System.out.println("1.- Descuento por edad "):
+            System.out.println("2.- convertir"):
+
+            System.out.println("Deseas repetir el programa?, Escribe s para si");
+            letra = entrada.next().charAt(0);
+
+            while(letra == 's' || letra = 'S');
+        }
+
+
+
+    }
+}
