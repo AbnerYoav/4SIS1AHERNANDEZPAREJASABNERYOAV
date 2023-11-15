a6d8fa30c4e95fbf69fe7f82fff87a8c317a7345
AbnerYoav <133718841+AbnerYoav@users.noreply.github.com>
2023-11-15 12:43:01 -0600

    Add files via upload

---
+++ P5F/src/main/java/com/mycompany/p5f/P5F.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.p5f;
+
+/**
+ *
+ * @author DELL
+ */
+public class P5F {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
